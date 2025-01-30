00e8bb1cbcf6cb8fba4eafe3d88fc9331e2ca406
miutamihai <mihaimiuta@proton.me>
2025-01-30 09:56:08 +0200

    feat: Add initial search screen

--- src/main.zig
+++ src/main.zig
@@ -5,16 +5,19 @@ const keys = @import("keys.zig");
 
 pub fn main() !void {
     const terminal_instance = try terminal.Terminal.init();
-    const screen_instance = screen.Screen.init(terminal_instance);
+    var screen_instance = screen.Screen.init(terminal_instance);
 
     try terminal_instance.clear_screen();
     try terminal_instance.enable_raw_mode();
     try terminal_instance.move_to_alt_screen();
-    const input_starting_coordinates = try screen_instance.home();
+    var input_starting_coordinates = try screen_instance.navigate(screen.ScreenType.home);
     try terminal_instance.move_cursor_to_coordinates(input_starting_coordinates);
     try terminal_instance.change_cursor_shape();
 
     const stdin = std.io.getStdIn().reader();
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = general_purpose_allocator.allocator();
+    var input = std.ArrayList(u8).init(gpa);
 
     while (true) {
         const byte = try stdin.readByte();
@@ -29,6 +32,12 @@ pub fn main() !void {
                 break;
             },
             .character => {
+                if (screen_instance.current_screen == screen.ScreenType.home) {
+                    input_starting_coordinates = try screen_instance.navigate(screen.ScreenType.search);
+                    try terminal_instance.move_cursor_to_coordinates(input_starting_coordinates);
+                }
+
+                try input.append(key.character);
                 try terminal_instance.write(&.{key.character});
             },
             .new_line => {
